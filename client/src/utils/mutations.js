ed613f533e7c731afd6c2eaeed32fcfc7c8cdacd
reinholz36 <dreinholz28@gmail.com>
2022-04-17 13:17:32 -0400

    updated mutations in client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: savedBook!) {
+      saveBook (input: $input)
+      {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+              bookId
+              authors
+              description
+              title
+              image
+              link
+          }
+      }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+      removeBook(bookId: $bookId) {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+              bookId
+              authors
+              description
+              title
+              image
+              link
+          }
+      }
+  }
+`;
